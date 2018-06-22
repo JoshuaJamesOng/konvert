576c2a238a49a7dac6451e864996460b63a93057
Joshua Ong <ong.joshuajames@gmail.com>
2018-06-22 01:28:29 +0100

    Remove old JobScheduler code

--- app/src/main/java/com/ongtonnesoup/konvert/di/UpdateExchangeRatesComponent.kt
+++ app/src/main/java/com/ongtonnesoup/konvert/di/UpdateExchangeRatesComponent.kt
@@ -1,13 +1,10 @@
 package com.ongtonnesoup.konvert.di
 
-import com.ongtonnesoup.konvert.currency.job.UpdateExchangeRatesService
 import com.ongtonnesoup.konvert.currency.work.UpdateExchangeRatesWorker
 import dagger.Subcomponent
 
 @Subcomponent
 interface UpdateExchangeRatesComponent {
-
-    fun inject(target: UpdateExchangeRatesService)
 
     fun inject(target: UpdateExchangeRatesWorker)
 
