84e661a22d501845ceb8c7274ddce31885ce339e
Joshua Ong <ong.joshuajames@gmail.com>
2018-10-01 22:48:20 +0100

    Hook up process lifecycle owner to create Dagger component on foreground

--- app/src/main/java/com/ongtonnesoup/konvert/KonvertApplication.kt
+++ app/src/main/java/com/ongtonnesoup/konvert/KonvertApplication.kt
@@ -1,6 +1,10 @@
 package com.ongtonnesoup.konvert
 
 import android.app.Application
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+import android.arch.lifecycle.ProcessLifecycleOwner
 import com.ongtonnesoup.konvert.currency.refresh.RefreshExchangeRatesWorker
 import com.ongtonnesoup.konvert.di.*
 import com.ongtonnesoup.konvert.initialisation.InitialiseApp
@@ -8,7 +12,10 @@ import com.ongtonnesoup.konvert.state.AppState
 import javax.inject.Inject
 import javax.inject.Provider
 
-class KonvertApplication : Application(), Provider<ApplicationComponent>, Injector<RefreshExchangeRatesWorker> {
+class KonvertApplication : Application(),
+        Provider<ApplicationComponent>,
+        Injector<RefreshExchangeRatesWorker>,
+        LifecycleObserver {
 
     @Inject
     lateinit var appState: AppState
@@ -26,23 +33,37 @@ class KonvertApplication : Application(), Provider<ApplicationComponent>, Inject
     override fun onCreate() {
         super.onCreate()
 
-        processComponent = DaggerProcessComponent.builder()
+        processComponent = createProcessComponent()
+
+        createAppComponentOnForeground()
+
+        initialiseApp()
+    }
+
+    private fun createProcessComponent(): ProcessComponent {
+        return DaggerProcessComponent.builder()
                 .processModule(ProcessModule(this))
                 .build()
+    }
 
+    private fun createAppComponentOnForeground() {
+        ProcessLifecycleOwner.get().lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            fun onAppForegrounded() {
+                if (applicationComponent == null) {
+                    applicationComponent = processComponent.getApplicationComponent()
+                }
+            }
+        })
+    }
+
+    private fun initialiseApp() {
         appState.updates()
                 .filter { state -> !state.initialised }
                 .flatMapCompletable { initialiseApp.initialise() }
                 .subscribeOn(schedulers.getWorkerScheduler())
                 .observeOn(schedulers.getPostExecutionScheduler())
                 .subscribe()
-    }
-
-    // TODO Hook this up to process lifecycle
-    fun onAppForegrounded() {
-        if (applicationComponent == null) {
-            applicationComponent = processComponent.getApplicationComponent()
-        }
     }
 
     override fun get() = applicationComponent
