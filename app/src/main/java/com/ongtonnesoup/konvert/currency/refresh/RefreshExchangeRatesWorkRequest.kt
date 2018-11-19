91af7f4cf9690483c8c0ce4b57e99f96c72c3373
Joshua Ong <ong.joshuajames@gmail.com>
2018-11-19 20:12:48 +0000

    Use inline functions to match KTX style

--- app/src/main/java/com/ongtonnesoup/konvert/currency/refresh/RefreshExchangeRatesWorkRequest.kt
+++ app/src/main/java/com/ongtonnesoup/konvert/currency/refresh/RefreshExchangeRatesWorkRequest.kt
@@ -7,16 +7,31 @@ import java.util.concurrent.TimeUnit
 class RefreshExchangeRatesWorkRequest(private val workManager: WorkManager) {
 
     fun schedule(): UUID? {
-        val constraint = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-        
-        val update = PeriodicWorkRequestBuilder<RefreshExchangeRatesWorker>(
-            1, TimeUnit.DAYS
-        ).setConstraints(constraint).build()
+        val constraint = ConstraintsBuilder {
+            setRequiredNetworkType(NetworkType.CONNECTED)
+        }
+
+        val update = PeriodicWorkRequest<RefreshExchangeRatesWorker>(1, TimeUnit.DAYS) {
+            setConstraints(constraint)
+        }
 
         workManager.enqueue(update)
 
         return update.id
+    }
+
+    private inline fun ConstraintsBuilder(func: Constraints.Builder.() -> Unit): Constraints {
+        val constraints = Constraints.Builder()
+        func.invoke(constraints)
+        return constraints.build()
+    }
+
+    private inline fun <reified T : Worker> PeriodicWorkRequest(
+            repeatInterval: Long,
+            repeatIntervalTimeUnit: TimeUnit,
+            func: PeriodicWorkRequest.Builder.() -> Unit): PeriodicWorkRequest {
+        val builder = PeriodicWorkRequestBuilder<T>(repeatInterval, repeatIntervalTimeUnit)
+        func.invoke(builder)
+        return builder.build()
     }
 }
